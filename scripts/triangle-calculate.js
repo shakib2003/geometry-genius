2d0c806946c1090fb24f0e956d6e66c2c154665f
shakib2003 <shakib2003skb@gmail.com>
2024-02-13 21:40:13 +0600

    geometry cards added and finished triangle calculation by javascript

---
+++ scripts/triangle-calculate.js
@@ -0,0 +1,12 @@
+function calculateTriangleArea(){
+  const triangleBaseInput = document.getElementById('triangle-base');
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+  const triangleHeightInput = document.getElementById('triangle-height');
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  const area = 0.5*base*height;
+  console.log("area of the triangle is: ", area);
+  const triangleAreaSpan = document.getElementById('triangle-area');
+  triangleAreaSpan.innerText = area;
+}
